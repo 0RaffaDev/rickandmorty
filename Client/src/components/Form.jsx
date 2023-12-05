71c289eef3e8f3b11c949de1c682574987f729e4
0RaffaDev <graffadev0@gmail.com>
2023-12-04 21:14:07 -0300

    Arreglo login style

--- Client/src/components/Form.jsx
+++ Client/src/components/Form.jsx
@@ -1,6 +1,6 @@
 
 import {Link } from 'react-router-dom';
-
+import login from '../img/7dxS.gif';
 
 const Form = ()=>{
   
@@ -10,15 +10,45 @@ const Form = ()=>{
 
         <div>
             <center>
-            <form >
-                <h1>LOGIN</h1>
-                <label htmlFor=""> Email: </label>
-                <input type="text"></input>
-                <label htmlFor=""> Contraseña: </label>
-                <input type="text"></input>
+            <form style={{
+    
+
+      height  : '500px'
+    , width: '300px'
+
+    , backgroundColor: 'rgba(0,0,0,0.5)'
+    , boxShadow: '100px'
+    , opacity : '10px'
+    , flexDirection  : 'column'
+    , justifyContent : 'center'
+    , alignItems     : 'center'
+    , margin: '100px'
+    , padding: '10px'
+
+
+   }} >
+
+                <h1 style={{}}>LOGIN</h1>
+               
                 <br />
                 <br />
-                    <Link to='/home'> <button>SUBMIT</button></Link>
+                <label htmlFor=""> Email:</label>
+                <br />
+                <input type="text"></input>
+                <br />
+               
+                <label htmlFor=""> Password: </label>
+                <br />
+                <input type="password"></input>
+                <br />
+                <br />
+                    <Link to='/home'> <button type='submit'> <img
+                 src={login} style={{width:'150px', height: '150px',}}
+                 ></img>
+                  <br />
+                  SUBMIT</button></Link>
+                
+                 
 
              
                 
